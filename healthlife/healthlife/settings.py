5669236c097ea2aab21c46011959d5b460f75f2c
flavio <flavio.drh@gmail.com>
2018-04-18 03:27:39 -0300

    inicio do projeto

--- healthlife/healthlife/settings.py
+++ healthlife/healthlife/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'healthlife.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'flavio',
+        'NAME': 'healthlife',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
